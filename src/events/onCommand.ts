45f29f05cbd89bc20cf8a6177287ea039b6e41ff
vylaz <kaffix1337@gmail.com>
2023-04-02 19:11:04 +0200

    x

--- src/events/onCommand.ts
+++ src/events/onCommand.ts
@@ -4,42 +4,54 @@ import { Interaction, PermissionsBitField } from "discord.js"
 
 export default {
     name: "interactionCreate",
-    execute: (client:Client, interaction: Interaction) => {
-        if(!interaction.isCommand())
+    execute: async (client: Client, interaction: Interaction) => {
+        if (!interaction.isCommand())
             return
 
+        await interaction.guild?.members.fetch({
+            user: interaction.user.id,
+            
+        })
+        const member = interaction.guild?.members.cache.get(interaction.user.id);
+
         const command = client.commands.get(interaction.commandName)
-        if(!command)
+        if (!command)
             return interaction.reply({
                 content: "Something broken!"
             })
 
-        if(command.devOnly && !client.config.devs.includes(interaction.user.id))
+        if (command.devOnly && !client.config.devs.includes(interaction.user.id))
             return interaction.reply({
                 content: "You are not a developer!"
             })
 
-        if(command.guildOnly && !interaction.guildId)
+        if (command.guildOnly && !interaction.guildId)
             return interaction.reply({
                 content: "You can use this command only in guild!"
             })
-        
-        if(interaction.guildId){
+
+        if (command.voiceOnly && !member?.voice.channel) {
+            return interaction.reply({
+                content: "You can use this command only in voice channels"
+            })
+        }
+
+        if (interaction.guildId) {
             let botPerms: any = command.permissions.bot
 
-            if(command.permissions.bot && !interaction.guild?.members.me?.permissions.has(PermissionsBitField.resolve(botPerms)))
+            if (command.permissions.bot && !interaction.guild?.members.me?.permissions.has(PermissionsBitField.resolve(botPerms)))
                 return interaction.reply({
                     content: `I don't have required permissions: \`${command.permissions.bot.join(`, `)}\``
                 })
 
             let userPerms: any = command.permissions.user
-            
-            if(command.permissions.user && !interaction.guild?.members.cache.get(interaction.user.id)?.permissions.has((PermissionsBitField.resolve(userPerms))))
+
+            if (command.permissions.user && !interaction.guild?.members.cache.get(interaction.user.id)?.permissions.has((PermissionsBitField.resolve(userPerms))))
                 return interaction.reply({
                     content: `You don't have required permissions: \`${command.permissions.user.join(`, `)}\``
                 })
         }
-        
+
         try {
             command.execute(client, interaction)
         } catch (err) {
