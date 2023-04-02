bfe9acbe2a4d07551257457e4a2aad6ff182450c
rayerr <n3gativ@outlook.com>
2023-04-02 18:52:35 +0200

    add button handler

---
+++ src/handlers/buttons.ts
@@ -0,0 +1,15 @@
+import Client from "../libs/client"
+import * as fs from "fs"
+import command from "../interfaces/command"
+import { greenBright, blueBright, redBright } from "chalk"
+
+export default async (client : Client) => {
+    await fs.readdirSync(`${process.cwd()}/src/buttons`).filter(f => !f.includes(`.`)).forEach(async btn=> {
+        const button = require(`${process.cwd()}/src/button/${btn}`).default
+
+        client.buttons.set(button.id, button)
+    })
+
+    // console.log(greenBright("Successful loaded "), blueBright(`${client.commands.size} `), greenBright("commands!"))
+    console.log(redBright("Successful loaded"), blueBright(`${client.buttons.size}`), redBright("buttons"))
+}
